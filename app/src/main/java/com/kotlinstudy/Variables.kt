5e86661019312fc64db8630234abde4b071723ce
JBarbosaM <jmateus.barbosa07@gmail.com>
2022-12-28 22:27:16 -0300

    - variables

---
+++ app/src/main/java/com/kotlinstudy/Variables.kt
@@ -0,0 +1,25 @@
+package com.kotlinstudy
+
+fun main(args: Array<String>){
+
+    var a = 10
+    var b = 20
+    var c = a + b
+
+    println(c)
+
+    var age = 10
+    var _age = 20
+    var age3 = 30
+
+  /*
+   VAL is immutable
+
+    val time = 100
+    time = 130
+
+   */
+
+    age = 20
+    println(age)
+}
